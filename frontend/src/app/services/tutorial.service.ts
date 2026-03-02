c9d939bfd2de6f718eb03b5dc892612d46cada6f
Ajay Yadav <yadavajay0529@gmail.com>
2026-03-02 17:44:25 +0000

    Initial MEAN Docker setup

---
+++ frontend/src/app/services/tutorial.service.ts
@@ -0,0 +1,43 @@
+import { environment } from '../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tutorial } from '../models/tutorial.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TutorialService {
+
+private baseUrl = '/api/tutorials';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Tutorial[]> {
+    return this.http.get<Tutorial[]>(this.baseUrl);
+  }
+
+  get(id: any): Observable<Tutorial> {
+    return this.http.get<Tutorial>(`${this.baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(this.baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(this.baseUrl);
+  }
+
+  findByTitle(title: any): Observable<Tutorial[]> {
+    return this.http.get<Tutorial[]>(`${this.baseUrl}?title=${title}`);
+  }
+}
